4af806ddb69792115e38b07bfa1ee21cb79f99e5
Aman Dwivedi <g25.aman.krishnadwivedi@gnkhalsa.edu.in>
2025-12-14 16:27:17 +0530

    make hero-alert mobile friendly

--- components/hero-alert.tsx
+++ components/hero-alert.tsx
@@ -1,8 +1,9 @@
 export default function HeroAlert() {
   return (
-    <div className="h-8 bg-blue-200/50 flex items-center justify-center rounded-full px-4 py-5">
-      <p className="text-sm text-blue-700 ">
-        <span className="inline-block w-2 h-2 bg-blue-500 rounded-full animate-pulse"></span> No signup required. Works in your browser.</p>
+    <div className="inline-flex items-center gap-2 bg-blue-200/50 text-blue-700 rounded-full px-3 py-2 sm:px-4 sm:py-2.5">
+      <span className="inline-block w-2 h-2 bg-blue-500 rounded-full animate-pulse"></span>
+      <p className="text-xs sm:text-sm font-medium whitespace-nowrap text-blue-700 ">
+        No signup required. Works in your browser.</p>
     </div>
   )
 }
